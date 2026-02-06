2d2f5e9d9ed58ce24c0410badb0d7984431948cb
Solessfir <34831419+Solessfir@users.noreply.github.com>
2026-02-06 21:38:43 +0100

    BP Async Action: LerpToAttachmentSocket

---
+++ Source/Toolbox/Private/AsyncActions/LerpToAttachmentSocket.cpp
@@ -0,0 +1,79 @@
+﻿// Copyright Solessfir. All Rights Reserved.
+
+#include "AsyncActions/LerpToAttachmentSocket.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+ULerpToAttachmentSocket_AsyncAction* ULerpToAttachmentSocket_AsyncAction::LerpToAttachmentSocket(USceneComponent* InComponent, const FName InTargetSocket, const FTransform InRelativeOffset, const float Duration)
+{
+    if (!InComponent)
+    {
+        return nullptr;
+    }
+
+    ULerpToAttachmentSocket_AsyncAction* AsyncAction = NewObject<ULerpToAttachmentSocket_AsyncAction>();
+    AsyncAction->SceneComponent = InComponent;
+    AsyncAction->TargetSocket = InTargetSocket;
+    AsyncAction->TargetTransform = InRelativeOffset;
+    AsyncAction->Duration = FMath::Max(0.001f, Duration);
+    return AsyncAction;
+}
+
+void ULerpToAttachmentSocket_AsyncAction::Activate()
+{
+    if (!SceneComponent || !SceneComponent->GetAttachParent())
+    {
+        OnFinished.Broadcast();
+        SetReadyToDestroy();
+        return;
+    }
+
+    // Register with Game Instance to prevent Garbage Collection while Ticking
+    RegisterWithGameInstance(SceneComponent->GetWorld());
+
+    // Attach immediately but keep World Position. This calculates the necessary Relative offset to the new Socket automatically
+    SceneComponent->AttachToComponent(SceneComponent->GetAttachParent(), FAttachmentTransformRules::KeepWorldTransform, TargetSocket);
+    StartTransformRelative = SceneComponent->GetRelativeTransform();
+    bActive = true;
+}
+
+void ULerpToAttachmentSocket_AsyncAction::Tick(float DeltaTime)
+{
+    if (!bActive || !SceneComponent)
+    {
+        return;
+    }
+
+    CurrentDuration += DeltaTime;
+    const float Alpha = FMath::Clamp(CurrentDuration / Duration, 0.f, 1.f);
+
+    // DualQuatInterp is used for smoother Rotation handling
+    const FTransform CurrentRelTransform = UKismetMathLibrary::TLerp(StartTransformRelative, TargetTransform, Alpha, ELerpInterpolationMode::DualQuatInterp);
+    SceneComponent->SetRelativeTransform(CurrentRelTransform);
+    OnUpdated.Broadcast();
+
+    if (Alpha >= 1.f)
+    {
+        bActive = false;
+        OnFinished.Broadcast();
+        SetReadyToDestroy();
+    }
+}
+
+bool ULerpToAttachmentSocket_AsyncAction::IsTickable() const
+{
+    return bActive && !HasAnyFlags(RF_BeginDestroyed);
+}
+
+TStatId ULerpToAttachmentSocket_AsyncAction::GetStatId() const
+{
+    RETURN_QUICK_DECLARE_CYCLE_STAT(ULerpToAttachmentSocket_AsyncAction, STATGROUP_Tickables);
+}
+
+void ULerpToAttachmentSocket_AsyncAction::DrawDebugLocation(const float Radius, const FLinearColor Color, const float DrawDuration) const
+{
+    if (SceneComponent)
+    {
+        UKismetSystemLibrary::DrawDebugSphere(SceneComponent->GetWorld(), SceneComponent->GetComponentLocation(), Radius, 12, Color, DrawDuration);
+    }
+}
---
+++ Source/Toolbox/Public/AsyncActions/LerpToAttachmentSocket.h
@@ -0,0 +1,59 @@
+﻿// Copyright Solessfir. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "Tickable.h"
+#include "LerpToAttachmentSocket.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLerpStep);
+
+UCLASS(Meta = (ExposedAsyncProxy = "AsyncAction"))
+class TOOLBOX_API ULerpToAttachmentSocket_AsyncAction : public UBlueprintAsyncActionBase, public FTickableGameObject
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Activate() override;
+
+    virtual void Tick(float DeltaTime) override;
+
+    virtual bool IsTickable() const override;
+
+    virtual TStatId GetStatId() const override;
+
+    /**
+    * Smoothly lerps a Scene Component to a new socket on its current parent
+    * @param InComponent Component to move
+    * @param InTargetSocket Socket name on the Parent to Attach to
+    * @param InRelativeOffset Additional Relative Transform Attach offset
+    * @param Duration How long the transition should take
+    */
+    UFUNCTION(BlueprintCallable, Meta = (BlueprintInternalUseOnly = true, AdvancedDisplay = "InRelativeOffset"), Category = "Transformation")
+    static ULerpToAttachmentSocket_AsyncAction* LerpToAttachmentSocket(USceneComponent* InComponent, const FName InTargetSocket, const FTransform InRelativeOffset, const float Duration = 0.25f);
+
+    UFUNCTION(BlueprintCallable, Category = "Transformation|Debug")
+    void DrawDebugLocation(float Radius = 10.f, FLinearColor Color = FLinearColor::Red, float DrawDuration = 0.1f) const;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnLerpStep OnUpdated;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnLerpStep OnFinished;
+
+private:
+    UPROPERTY()
+    TObjectPtr<USceneComponent> SceneComponent;
+
+    FName TargetSocket;
+
+    float Duration = 0.f;
+
+    FTransform TargetTransform;
+
+    float CurrentDuration = 0.f;
+
+    bool bActive = false;
+
+    FTransform StartTransformRelative;
+};
